057f3eaff872d3b0032b30df0f3e917047957947
hidejxz <hidejxz@hotmail.com>
2018-06-13 22:46:25 +0800

    20180613

--- Regression.py
+++ Regression.py
@@ -15,6 +15,14 @@ def load_dataset(filename):
         label_mat.append(float(cur_line[-1]))
     return data_mat, label_mat
 
+def preprocessing(X, y):
+    y_mean = mean(y,axis = 0)
+    y_arr = y - y_mean
+    x_mean = mean(X,axis = 0)
+    x_var = var(X,axis = 0)
+    x_arr = (X-x_mean)/x_var
+    return x_arr, y_arr
+
 class Regression():
 
 
@@ -47,5 +55,39 @@ class Regression():
             y_hat.append(result[0,0])
         return y_hat
 
+    def ridge_regression(self, X, y, lam = 0.2):
+        X = mat(X)
+        y = mat(y).T
+        denom = X.T*X + eye(shape(X)[1])*lam
+        if linalg.det(denom) == 0.0:
+            print('Error')
+            return
+        w = denom.I * X.T * y
+        return w
 
+    def rss_error(self, y, y_hat):
+        return ((array(y) - array(y_hat))**2).sum()
+
+    def stage_wise(self, X, y, eps = 0.01, iter = 100):
+        X = mat(X)
+        y = mat(y).T
+        m,n = shape(X)
+        w_mat = mat(zeros((iter,n)))
+        ws = mat(zeros((n,1)))
+        ws_max = ws.copy()
+        for i in range(iter):
+            print(ws)
+            lowest_error = inf 
+            for j in range(n):
+                for sign in [-1, 1]:
+                    ws_test = ws.copy()
+                    ws_test[j] += eps*sign
+                    y_test = X * ws_test
+                    rsse = self.rss_error(y,y_test)
+                    if rsse < lowest_error:
+                        lowest_error = rsse
+                        ws_max = ws_test
+                ws = ws_max.copy()
+            w_mat[i] = ws.T
+        return w_mat.round(4)
 
